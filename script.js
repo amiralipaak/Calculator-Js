d4c7e3e9ad951295da5f8a3c2cc61632e08bf555
amiralipaak <amiralipaak380@gmail.com>
2022-06-03 21:01:11 +0430

    Updated the HTML and CSS and added new JS codes

---
+++ script.js
@@ -0,0 +1,9 @@
+class Calculator {}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const POTextElement = document.querySelector("[data-previous-operation]");
+const COTextElement = document.querySelector("[data-current-operation]");
